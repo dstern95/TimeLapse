76a1b078570ffa70beee5543092857af37270387
pmathur <pmathur@fandm.edu>
2017-04-05 02:26:30 -0400

    Latest

--- app/src/main/java/com/example/timelapse/PhotoFiles.java
+++ app/src/main/java/com/example/timelapse/PhotoFiles.java
@@ -25,7 +25,7 @@ public class PhotoFiles extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_files);
-        File loc[] = new File(Environment.getExternalStorageDirectory() + "/" + "timelapsefiles").listFiles();
+        File loc[] = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES) + "/" + "Timelapse").listFiles();
         fileArray = new String[loc.length];
 
         for (int i = 0; i < loc.length; i++) {
@@ -41,7 +41,7 @@ public class PhotoFiles extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String filename = listView.getItemAtPosition(position).toString();
 
-                Intent i = new Intent(PhotoFiles.this, PhotoView.class);
+                Intent i = new Intent(PhotoFiles.this, Play.class);
                 i.putExtra("foldername", filename);
 
                 startActivity(i);
